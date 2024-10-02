7abaf38c8730d1a46ef2fae45111a10894d5d7a3
Pratham Raje <s4007909@student.rmit.edu.au>
2024-10-02 23:15:25 +1000

    editing add.php

--- a2/add.php
+++ a2/add.php
@@ -4,15 +4,15 @@
 
 
 <body>
+<div class="wrapper">
 <?php include 'nav.inc'; ?>
     
     <h2 class="headings" > Add A Pet </h2>
     <p class="paragraphs">You can add a new pet here </p>
 
-    <form method="post" action="insert.php" enctype="multipart/form-data">
-        <label for="pet-name">Pet Name:</label><br>
-        <input type="text" id="pet-name" name="pet-name" placeholder="Please provide pet name below"><br>
-
+    <form action="process_add_pet.php" method="post" enctype="multipart/form-data">
+    <label for="petname">Pet Name:</label>
+    <input type="text" id="petname" name="petname" required><br><br>
 
  <label for="pet-type">Type:</label><br>
         <select id="pet-type" name="pet-type">
@@ -22,30 +22,29 @@
            
         </select><br>
         
-        <label for="description">Description:</label><br>
-        <textarea id="description" name="description" placeholder="Briefly describe the pet." rows="4" cols="50"></textarea><br>
+        <label for="description">Pet Description:</label>
+    <textarea id="description" name="description" rows="4" cols="50" required></textarea><br><br>
 
-
-
-        <label for="image">Select an image: <span class="mandatory">*</span></label>
+    <label for="image">Pet Image: <span class="mandatory">*</label>
         
         <div class="file-upload">
             <input type="file" id="image" name="image" accept="image/*" required>
             <span class="max-size">MAX IMAGE SIZE 500px</span>
         </div>
 
-        
-         <label for="image-caption">Image Caption: <span class="mandatory">*</span></label><br>
-         <input type="text" id="image-caption" name="image-caption" placeholder="Describe the image in one word" required><br>
- 
-       
-        
-        <label for="age">Age (months): <span class="mandatory">*</span></label><br>
-        <input type="text" id="age" name="age" placeholder="Age of pet in months" required><br>
 
-    
-        <label for="location">Location: <span class="mandatory">*</span></label><br>
-        <input type="text" id="location" name="location" placeholder="Location of the pet" required><br>
+        <label for="caption">Image Caption: <span class="mandatory">*</span></label><br>
+        <input type="text" id="caption" name="caption" placeholder="Describe the image in one word" required><br><br>
+
+
+        <label for="age">Age (months): <span class= "mandatory">*</span></label><br>
+    <input type="number" id="age" name="age" placeholder="Age of pet in months" required><br><br>
+
+
+
+    <label for="location">Location: <span class= "mandatory">*</span></label><br>
+    <input type="text" id="location" name="location" placeholder="Location of the pet" required><br><br>
+
 
         <div class="form-buttons">
             <button type="submit" class="submit-btn" value="Insert">
@@ -66,12 +65,15 @@
 
 
       <?php include 'footer.inc'; ?>
-    <!-- <footer class="footer">
+  <!-- <footer class="footer">
         <p>© COPYRIGHT S4007909. ALL RIGHTS RESERVED | DESIGNED FOR PETS VICTORIA <br>
        
       </footer> -->
 
-
+</div>
 </body>
 
-</html>
+
+
+
+
