a44fd8c15bfe8e244e31a3f21f4c14f7aa20f4e8
Pratham Raje <s4007909@student.rmit.edu.au>
2024-10-02 23:21:59 +1000

    editing gallery

--- a2/gallery.php
+++ a2/gallery.php
@@ -1,102 +1,59 @@
 <?php include 'header.inc'; ?>
-
 <?php 
-$pets = mysqli_query($conn, "select * from petname");
+
+include 'db_connect.inc'; 
+
+// Fetch pet data from the database
+$query = "SELECT petid, petname, image FROM pets";
+$result = mysqli_query($conn, $query);
 ?>
 
-
-
-
 <body>
+<div class="wrapper">
 
 <?php include 'nav.inc'; ?>
 
-    <h2 class="headings"> Pets Victoria Has A Lot To Offer! </h2>
-    <p class="paragraphs">For almost two decades, Pets Victoria has helped in creating true social change by bringing pet adoption into the mainstream. Our work has helped make a <br>difference to the Victorian rescue community and thousands of pets in need of rescue and rehabilitation. But, until every pet is safe, respected, and loved, we all <br> still have big, hairy work to do.</p>
-    
+<h2 class="headings"> Pets Victoria Has A Lot To Offer! </h2>
+<p class="paragraphs">For almost two decades, Pets Victoria has helped in creating true social change by bringing pet adoption into the mainstream. Our work has helped make a <br>difference to the Victorian rescue community and thousands of pets in need of rescue and rehabilitation. But, until every pet is safe, respected, and loved, we all <br> still have big, hairy work to do.</p>
+
+
+<aside>
+
+<div class="image-grid">
+    <?php
+    // Loop through each pet in the database
+    while($row = mysqli_fetch_assoc($result)) {
+        $petid = $row['petid'];
+        $petname = $row['petname'];
+        $image = $row['image']; // Image path stored in DB, e.g. "images/cat1.jpeg"
 
 
 
-    
-
-
-    <div>
-    <div class="image-grid">
+        echo '
         <div class="image-container">
-            <img src="cat1.jpeg" alt="Image 1">
-            <p class="pgallery">Milo</p>
+            <img src="' . $image . '" alt="' . $petname . '" style="width: 400px; height: 220px;">
+            <p class="pgallery">' . $petname . '</p>
             <div class="hover-text">
                 <div class="material-symbols-outlined">
                     search
                 </div>
-                <span>Discover more!</span>
+                <span><a href="details.php?id=' . $petid . '">Discover more!</a></span>
             </div>
-        </div>
-        <div class="image-container">
-            <img src="dog1.jpeg" alt="Image 2">
-            <p class="pgallery">Baxter</p>
-            <div class="hover-text">
-                <div class="material-symbols-outlined">
-                    search
-                </div>
-                <span>Discover more!</span>
-            </div>
-        </div>
-        <div class="image-container">
-            <img src="cat2.jpeg" alt="Image 3">
-            <p class="pgallery">Luna</p>
-            <div class="hover-text">
-                <div class="material-symbols-outlined">
-                    search
-                </div>
-                <span>Discover more!</span>
-            </div>
-        </div>
-        <div class="image-container">
-            <img src="dog2.jpeg" alt="Image 4">
-            <p class="pgallery">Willow</p>
-            <div class="hover-text">
-                <div class="material-symbols-outlined">
-                    search
-                </div>
-                <span>Discover more!</span>
-            </div>
-        </div>
-        <div class="image-container">
-            <img src="cat4.jpeg" alt="Image 5">
-            <p class="pgallery">Oliver</p>
-            <div class="hover-text">
-                <div class="material-symbols-outlined">
-                    search
-                </div>
-                <span>Discover more!</span>
-            </div>
-        </div>
-        <div class="image-container">
-            <img src="dog3.jpeg" alt="Image 6">
-            <p class="pgallery">Bella</p>
-            <div class="hover-text">
-                <div class="material-symbols-outlined">
-                    search
-                </div>
-                <span>Discover more!</span>
-            </div>
-        </div>
-    </div>
-</div>
- 
-
-
-   
-
-    <div>
-    <aside class= "bottompage">
-<?php include 'footer.inc'; ?>
+        </div>';
+    }
+    ?>
 
 </div>
 
+<aside>
+
+<div>
+<aside class="bottompage">
+    <?php include 'footer.inc'; ?>
+</aside>
+</div>
+
+</div>
 </body>
-
-
 
 </html>
